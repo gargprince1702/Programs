6b6b2ed9df2c0f0b3db2b8c04e0514d1ec4df54b
Prince Garg <54926566+gargprince1702@users.noreply.github.com>
2020-02-20 19:30:03 -0800

    Create solutionbygargprince1702.cpp

---
+++ Intermediate/Check if A Number is Prime or Not/solutionbygargprince1702.cpp
@@ -0,0 +1,24 @@
+//A Program to check whether a Number is Prime Or Not.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter a positive number to check Prime or Not ";
+	cin>>n;
+  int flag = 0;
+	for(int i=2;i<n;i++)
+	{
+		if(n%i==0)
+		flag+=1;
+	}
+	if(flag==0)
+	{
+		cout<<"Yes"; //If a Given number is Prime
+	}
+	else
+	{
+		cout<<"No"; // If a Given number is not Prime
+	}	
+}
+//This code is contributed by gargprince1702
